69c458cac85dd1012af4731b50e751fd2a625009
Khan 'Sadh' Mostafa <khan@nafSadh.com>
2013-11-25 02:30:44 +0000

    Common header with global defines

---
+++ IshikaCon/IshikaCon/IshikaStdInc.h
@@ -0,0 +1,35 @@
+#pragma once
+#ifndef ISHIKA_STD_INC
+#define ISHIKA_STD_INC
+
+#include <iostream>
+#include <gl/glut.h>
+
+#define X 0
+#define Y 1
+#define DIM 2
+#define STROKE_PTS 15000
+#define STROKE_CNT 5000
+#define STAMPS 15000
+#define SPLATS 15000
+
+#define StampDelta 0.01
+#define SplatDelta 0.05
+
+//window def
+#define WIDTH 1200
+#define HEIGHT 800
+#define RATIO 100
+#define FACTX 1000
+
+extern GLfloat theta; 
+extern GLfloat trans; 
+extern GLfloat Xmin;
+extern GLfloat Xmax;
+extern GLfloat Ymin; 
+extern GLfloat Ymax;
+extern GLint const xmid;
+extern GLint const ymid;
+extern GLboolean bRotate;
+
+#endif ISHIKA_STD_INC
